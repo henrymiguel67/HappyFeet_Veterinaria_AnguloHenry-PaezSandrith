a0b35ca4936c7abe8f39e8af50995c799e84e08b
sandrypaez <sandrypaez0727@gmail.com>
2025-09-25 15:33:03 -0500

    fix: :bug: Corregir validación de datos en agregarDueno

--- IdeaProjects/HappyFeet/src/main/java/controller/duenoController.java
+++ IdeaProjects/HappyFeet/src/main/java/controller/duenoController.java
@@ -1,7 +1,31 @@
 package controller;
-import
+
+import org.example.model.entities.dueno;
+import org.example.model.entities.iduenoDAO;
 
 public class duenoController {
-    private iduenoDAO
+    private iduenoDAO duenoDAO;
 
+    public duenoController(iduenoDAO duenoDAO) {
+        this.duenoDAO = duenoDAO;
+    }
+
+    public void agregarDueno(dueno dueno) {
+        if (validarDueno(dueno)) {
+            duenoDAO.agregarDueno(dueno);
+        }
+    }
+
+    private boolean validarDueno(dueno dueno) {
+        if (dueno == null) return false;
+
+        if (dueno.getNombre() == null || dueno.getNombre().isEmpty())
+            return false;
+
+        else (dueno.getTelefono() == null || dueno.getTelefono().isEmpty())
+            return false;
+
+        return true;
+    }
 }
+
